5e1d2f2bc15c916bad78095a5b21b80259a8da4e
QuangRau <lethanhquang910@gmail.com>
2014-10-20 01:32:50 +0700

    push to gh-pages

--- test/e2eSpecs/page.e2espec.js
+++
@@ -1,7 +0,0 @@
-describe('home page title', function() {
-    var ptor = protractor.getInstance();
-    it('should be lazada', function() {
-        ptor.get('/#');
-        expect(ptor.getTitle()).toBe('lazada');
-    });
-});
